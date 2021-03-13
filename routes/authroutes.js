a093780df6e149d834fb79d147b8c742b9a94f9c
Himanshu <himanshuranjan30@gmail.com>
2021-03-13 12:36:43 +0530

    google auth added

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -5,11 +5,30 @@ const passport=require('passport')
 const router = express.Router();
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const config=require('../config')
+const User = require("../models/user");
 var userProfile;
 
 
 
-router.get('/success', (req, res) => res.send(userProfile));
+router.get('/success', (req, res) => {res.send(userProfile)
+const userdetails={
+    name=userProfile.user.displayName,
+    email=userProfile.user.emails[0].value
+}
+const user=new User(userdetails)
+
+try {
+    await user.save();
+      return res.status(200).json({
+        message: "Successfully signed up!",
+      });
+    } catch (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+
+});
 router.get('/error', (req, res) => res.send("error logging in"));
 
 passport.serializeUser(function(user, cb) {

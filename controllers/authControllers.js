e751e9acff78103da101b068e24072f27c516616
Himanshu <himanshuranjan30@gmail.com>
2021-03-13 19:47:06 +0530

    auth tweaks and middlewares opt.

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,10 +3,6 @@ const jwt = require("jsonwebtoken");
 const expressJwt = require("express-jwt");
 const config = require("../config");
 
-
-
-
-
 const signin = async (req, res) => {
   try {
     let user = await User.findOne({
@@ -54,21 +50,22 @@ const signout = (req, res) => {
   });
 };
 
-const requireSignin = (req,res,next)=>{
-  
-  if(expressJwt({
-  secret: config.jwtSecret,
-  userProperty: "auth",
-  algorithms:['sha1', 'RS256', 'HS256']
-})||req.session.user)
-    next()
-
-}
+const requireSignin = (req, res, next) => {
+  if (
+    expressJwt({
+      secret: config.jwtSecret,
+      userProperty: "auth",
+      algorithms: ["sha1", "RS256", "HS256"],
+    }) ||
+    req.session.user
+  )
+    next();
+};
 
 const hasAuthorization = (req, res, next) => {
   const authorized = req.profile && req.auth && req.profile._id == req.auth._id;
-  const googleauthorized=req.session.user
-  if (authorized==0&&googleauthorized==0) {
+  const googleauthorized = req.session.user;
+  if (authorized == 0 && googleauthorized == 0) {
     return res.status("403").json({
       error: "User is not authorized",
     });
@@ -76,11 +73,9 @@ const hasAuthorization = (req, res, next) => {
   next();
 };
 
-
-module.exports={
+module.exports = {
   signin,
   signout,
   requireSignin,
   hasAuthorization,
-
 };

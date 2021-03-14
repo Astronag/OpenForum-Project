32352fa8837c1b84e6707c19fbd0f315e1cbc97e
Himanshu <himanshuranjan30@gmail.com>
2021-03-14 21:38:40 +0530

    dir path added

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -266,8 +266,8 @@ const trendingposts = (req, res) => {
         error: err,
       });
     }
-    res.json(posts)
-  
+    var result=JSON.parse(res.json())
+    return res.redirect("/api/users/"+result["postedBy"])
   });
 };
 

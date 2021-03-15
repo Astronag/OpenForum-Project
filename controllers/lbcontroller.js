df0d086c112f27ed70356835667dee0e34c09266
Himanshu <himanshuranjan30@gmail.com>
2021-03-16 00:13:17 +0530

    async bug fixed

--- controllers/lbcontroller.js
+++ controllers/lbcontroller.js
@@ -1,56 +1,46 @@
+  
 const Post = require("../models/post");
-const user = require("../models/user");
 const User = require("../models/user");
 
-const leaderboard = async (req, res) => {
-  await User.updateMany({
-    $set: { score: 0 },
-  }).exec(async (err, posts) => {
-    await Post.find({}, async function (err, docs) {
-      console.log(docs.length);
-      docs.forEach(async function (data) {
-        var userid = data.postedBy;
-        var updatedscore = data.score;
-        console.log(updatedscore);
-      
+const leaderboard = (req, res) => {
+  Post.find({}, function (err, docs) {
+    docs.forEach(function (data) {
+      var userid = data.postedBy;
+      var likes = data.likes.length;
     
-    await User.findByIdAndUpdate(
-      userid,
-      { $inc: { score: updatedscore } },
-      async function (errr, doc) {
-        if (errr) {
-          console.log(err);
-        } else {
-          console.log("Updated User : ", doc);
+      var comments = data.comments.length;
+      console.log(comments);
+      const date2 = new Date();
+      const diffTime = Math.abs(date2 - data.created);
+      const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+      console.log(diffDays);
+      const updated = User.findByIdAndUpdate(
+        userid,
+        {score: (likes+comments)/diffDays },
+        function (errr, doc) {
+          if (errr) {
+            console.log(err);
+          } else {
+            console.log("Updated User : ", doc);
+          }
         }
-      }
-    ).exec(async (er, result2) => {
-      await User.find({}, async function (error, doc) {
-        doc.forEach(async function (data) {
-          var userid = data.id;
-          var totalscore = data.score;
-          var tier = "Level 1 Contributor";
-          if (totalscore >= 500) tier = "Level 3 Contributor";
-          else if (totalscore >= 200 && totalscore < 500)
-            tier = "Level 2 Contributor";
-
-          User.findByIdAndUpdate(userid, {
-            $set: {
-              badge: tier,
-            },
-          }).exec();
-        });
-      }).exec(async (e, result3) => {
-        var mysort = { score: -1 };
-        await User.find()
-          .sort(mysort)
-          .exec((err, result) => {
-            if (err) throw err;
-            else res.json(result);
-          });
-      });
+      );
+      var mysort = { score: -1 };
+      User.find().sort(mysort).exec((err,result)=>{
+        if (err) throw err;
+        else
+           res.json(result)
+      })
+    
     });
-  }) 
-})})}
+  }).exec((err, posts) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+  
+  });
+};
 
-module.exports = { leaderboard }
+module.exports = { leaderboard };
